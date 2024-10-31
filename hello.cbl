fd47eeae5562b26327f46798ee9b65af216cf0e3
Andrew Hughes <andrew.hughes1@ibm.com>
2024-10-31 15:35:48 +0000

    bump COBOL app text

--- hello.cbl
+++ hello.cbl
@@ -3,7 +3,7 @@
         ENVIRONMENT DIVISION.
         DATA DIVISION.
         WORKING-STORAGE SECTION.
-        01 MSG  PIC X(13) VALUE "Hello CAF 24!".
+        01 MSG  PIC X(18) VALUE "Hello GSE UK 2024!".
         LINKAGE SECTION.
         PROCEDURE DIVISION.
             DISPLAY "Hello".
