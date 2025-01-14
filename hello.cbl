75fd5a104e2a72776330b25af290eef839aed85b
Andrew Twydell <andrew.twydell@ibm.com>
2025-01-14 15:51:45 +0000

    update hello.cbl content

--- hello.cbl
+++ hello.cbl
@@ -3,7 +3,7 @@
         ENVIRONMENT DIVISION.
         DATA DIVISION.
         WORKING-STORAGE SECTION.
-        01 MSG  PIC X(18) VALUE "Hello GSE UK 2024!".
+        01 MSG  PIC X(13) VALUE "Hello, World!".
         LINKAGE SECTION.
         PROCEDURE DIVISION.
             DISPLAY "Hello".
